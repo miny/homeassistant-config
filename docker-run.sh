2f9e18cb3427233f7208173351899c93d81d1082
miny <miny@minabe.org>
2018-03-08 12:37:01 +0900

    home assistant config files.

---
+++ docker-run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+docker run --detach \
+    --hostname hahost \
+    --name="home-assistant" \
+    --restart always \
+    --publish 8123:8123 \
+    --volume /home/hass/config:/config \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /dev/bus/usb:/dev/bus/usb \
+    --device /dev/vhci:/dev/vhci \
+    --privileged \
+    --net=host \
+    homeassistant/home-assistant
+
