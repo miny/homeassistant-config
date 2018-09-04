cbdeaa992306ba9e8cb2b2886caa535eae5d262a
miny <miny@minabe.org>
2018-09-04 13:37:35 +0900

    update docker run script.

--- docker-run.sh
+++ docker-run.sh
@@ -4,12 +4,11 @@ docker run --detach \
     --hostname hahost \
     --name="home-assistant" \
     --restart always \
-    --publish 8123:8123 \
     --volume /home/hass/config:/config \
     --volume /etc/localtime:/etc/localtime:ro \
     --volume /dev/bus/usb:/dev/bus/usb \
     --device /dev/vhci:/dev/vhci \
     --privileged \
     --net=host \
-    homeassistant/home-assistant:0.74.2
+    homeassistant/home-assistant:0.76.2
 
